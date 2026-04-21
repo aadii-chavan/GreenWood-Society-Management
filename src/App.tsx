8db4217b0b0ee84777ee31801cfc3ffce358f19c
Ethan <theupsidedownportal@gmail.com>
2026-04-22 03:38:10 +0530

    dark mode toggle with theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/components/theme-provider";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import Residents from "./pages/Residents.tsx";
@@ -17,24 +18,26 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/residents" element={<Residents />} />
-          <Route path="/bills" element={<Bills />} />
-          <Route path="/complaints" element={<Complaints />} />
-          <Route path="/visitors" element={<Visitors />} />
-          <Route path="/notices" element={<Notices />} />
-          <Route path="/settings" element={<Settings />} />
+    <ThemeProvider defaultTheme="light" storageKey="society-hub-theme">
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/residents" element={<Residents />} />
+            <Route path="/bills" element={<Bills />} />
+            <Route path="/complaints" element={<Complaints />} />
+            <Route path="/visitors" element={<Visitors />} />
+            <Route path="/notices" element={<Notices />} />
+            <Route path="/settings" element={<Settings />} />
 
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </ThemeProvider>
   </QueryClientProvider>
 );
 
